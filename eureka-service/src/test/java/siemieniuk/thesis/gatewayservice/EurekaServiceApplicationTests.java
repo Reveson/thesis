f76aca8b15ca76ec11508e4f0cbb064895caad30
mateusz.siemieniuk <siemieniuk.mat@gmail.com>
2021-09-16 20:44:19 +0200

    Eureka service discovery

---
+++ eureka-service/src/test/java/siemieniuk/thesis/gatewayservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package siemieniuk.thesis.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
